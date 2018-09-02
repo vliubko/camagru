6c4c19c83797d80acf2de3a086ece9032bb7d5d1
vliubko <neznam.ua@gmail.com>
2018-09-02 17:24:39 +0300

    Add login js + login css

---
+++ app/js/login_script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+        $("#form-signin").submit(function(e){
+            e.preventDefault();
+    
+            var login = $.trim($("#login").val());
+            var password = $.trim($("#password").val());
+    
+            if(login == '' || password == '') {
+                $("img.profile-img").attr("src", "/data/images/error.png");
+            } else {
+                $("img.profile-img").attr("src", "/data/images/ok.png");
+                $(this).unbind().submit();
+            }
+        });
+    
+    });
