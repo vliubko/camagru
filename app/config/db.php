702020b2e6edcf2024a7a8181741b8777d51af06
vliubko <neznam.ua@gmail.com>
2018-09-12 16:00:08 +0300

    MyPDO refactor

--- app/config/db.php
+++ app/config/db.php
@@ -1,5 +1,13 @@
 <?php
 
+class MyPDO extends PDO {
+    public function run($sql, $args = NULL) {
+        $stmt = $this->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+}
+
 Class DB {
 
     public static function connToDB() {
@@ -9,10 +17,14 @@ Class DB {
         $dsn = 'mysql:dbname='.$dbname.';host='.$host;
         $user = $_SERVER['MYSQL_ROOT_USER'];
         $pass = $_SERVER['MYSQL_ROOT_PASSWORD'];
-        
+
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
         try {
-            $connection = new PDO($dsn, $user, $pass);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $connection = new MyPDO($dsn, $user, $pass, $opt);
         }
         catch (PDOException $e) {
             echo 'Error: ' . $e->getMessage();
