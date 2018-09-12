5f023b02f52c06d6ba0e978028cec0e34e150db1
Vadym Liubko <vadym.liubko@gmail.com>
2018-09-12 13:44:32 +0300

    upd

--- app/views/account/settings.tpl.php
+++ app/views/account/settings.tpl.php
@@ -15,7 +15,23 @@
 
     <br>
 
-    <a href="/account/sendmail"> Send mail </a>
+    <form id="settings" method="post">
+            <?php if(!empty($pageData['error'])) :?>
+                <p><?php echo $pageData['error']; ?></p>
+            <?php endif; ?>
+            <p> Username</p>
+            <input type="text" name="user" id="user" placeholder="New Username" autofocus><br>
+
+            <p> Email </p>
+            <input type="email" name="email" id="email" placeholder="New Email" required><br>
+
+            <p> Password </p>
+            <input type="password" name="password" id="password" placeholder="New Password" required><br>
+        <button type="submit">
+            Change
+        </button>
+    </form>
+
     <a href="/account/logout">Logout</a> <br>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
