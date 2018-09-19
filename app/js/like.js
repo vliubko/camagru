8abcfda41d38207317cc437197be5aa2404d79b3
vliubko <neznam.ua@gmail.com>
2018-09-19 17:58:10 +0300

    js fix

--- app/js/like.js
+++ app/js/like.js
@@ -8,7 +8,7 @@ for (i=0; i<likeButton.length; i++) {
         var likeFullId = this.getAttribute('id');
         var likeId = likeFullId.substr(9);
         // pressLike(likeId)
-        sendRequest(likeId)
+        sendRequest(this)
         e.preventDefault()
     })
 };
@@ -17,9 +17,12 @@ for (i=0; i<likeButton.length; i++) {
 //     sendRequest(likeId);
 // }
 
-function sendRequest(likeId) {
+function sendRequest(elem) {
     let XHR = new XMLHttpRequest()
     let likeData = new FormData();
+
+    var likeFullId = elem.getAttribute('id');
+    var likeId = likeFullId.substr(9);
 
     likeData.append('like-id', likeId)
 
@@ -27,6 +30,7 @@ function sendRequest(likeId) {
         console.log("XHR loaded")
         console.log("Like id is:", likeId)
         console.log('answer = > ', event.target.responseText)
+        elem.src = "/data/images/like_full.png"
     })
     XHR.open("POST", 'like');
     XHR.send(likeData)
