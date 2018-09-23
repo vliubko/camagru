c81b063d216c657617c95f9ad1330205a2c51d06
Vadym Liubko <vadym.liubko@gmail.com>
2018-09-23 21:09:29 +0300

    solo photo id

--- app/views/account/show_photo.tpl.php
+++ app/views/account/show_photo.tpl.php
@@ -26,40 +26,57 @@
 <?php
     $photo = $pageData['photo'];
     echo "<div class=\"photos\">";
-    echo "<div class=\"photo\" id=\"photo-". $photo['id']. "\">\n";
-    echo "<img height=20px src=\"/data/images/user-shape.png\">";
-    echo " " . $photo['username']."<br>";
-    echo "<img class=\"img-photo\" height=300px src=" . $photo['url'] . "><br>\n";
-    echo "<div class=\"like\">";
-    if (!empty($photo['like_status'])) {
-        echo "<img class=\"like-img\" height=20px src=\"/data/images/like_full.png\">";
-    } else {
-        echo "<img class=\"like-img\" height=20px src=\"/data/images/like.png\">";
-    }
-    echo "</div>";
-    echo "<div class=\"number-likes\">" . $photo['likes'] . "</div>";
+        echo "<div class=\"photo\" id=\"photo-". $photo['id']. "\">\n";
+            echo "<img height=20px src=\"/data/images/user-shape.png\">";
+            echo " " . $photo['username']."<br>";
+            echo "<img class=\"img-photo\" height=300px src=".$photo['url']."><br>\n";
 
-    foreach ($photo['comments'] as $comment) {
-        echo "<div class=\"comment\">";
-        echo $comment['username'] . " ";
-        echo $comment['message'];
-        echo "</div>";
-    }
+            echo "<div class=\"number-likes\">";
+                echo $photo['likes'];
+            echo "</div>";
+            
+            echo "<div class=\"like\">";
+            echo "<img class=\"like-img\" height=20px src=\"/data/images/";
+            if (!empty($photo['like_status'])) {
+                echo "like_full.png\">";
+            } else {
+                echo "like.png\">";
+            }
+            echo "</div>";
 
-    echo "<div class=\"text-date\">". $photo['timeAgo'] . "</div>";
-    echo "<hr>";
-    echo "<form id=\"comment_form\">";
-    echo "<input
-    onkeypress=\"insertComment(event, '', '1')\"
-    id=\"comment_text_1\" placeholder=\"Enter a comment...\" class=\"comment_text\"></input>";
-    echo "</form>";
-    echo "<button type=\"submit\" form=\"comment_form\" value=\"Submit\">Add</button>";
-    echo "\n</div>\n";
+            echo "<div class=\"comment-img-div\">";
+                echo "<a href=\"/photo/". $photo['id'] . "\">";
+                    echo "<img class=\"comment-img\" height=20px src=\"/data/images/comment.png\">";
+                echo "</a>";
+            echo "</div>";
+
+            echo "<div class=\"comments\" id=\"comments_div_" . $photo['id'] . "\">";
+                foreach ($photo['comments'] as $comment) {
+                    echo "<div class=\"comment\">";
+                        echo $comment['username'] . " ";
+                        echo $comment['message'];
+                    echo "</div>";
+                }
+            echo "</div>";
+
+            echo "<div class=\"text_date\">";
+                echo $photo['timeAgo'];
+            echo "</div>";
+
+            echo "<hr>";
+            echo "<div class=\"new_comment_div\">";
+                echo "<form id=\"comment_form_" . $photo['id'] . "\">";
+                echo "<input id=\"comment_text_" . $photo['id'] . "\" placeholder=\"Enter a comment...\" class=\"comment_text\"></input>";
+                echo "<input id=\"comment_button_" . $photo['id'] . "\" type=\"submit\" value=\"Add\"></input>";
+                echo "</form>";
+            echo "\n</div>\n";
+        echo "\n</div>\n";
     echo "\n</div>\n";
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="/js/login_script.js"></script>
     <script src="/js/like.js"></script>
+    <script src="/js/comment.js"></script>
 </body>
 </html>
